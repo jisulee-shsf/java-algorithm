e04e4a8b5e0f974f480ed8a981738afc628b2ab3
Jisu Lee <jisulee.shsf@gmail.com>
2023-06-13 23:23:49 +0900

    Upload file

---
+++ chap_01/String1.java
@@ -0,0 +1,25 @@
+package chap_01;
+
+import java.util.Scanner;
+
+public class String1 {
+    public int solution(String str, char c) {
+        int answer = 0;
+        str = str.toUpperCase();
+        c = Character.toUpperCase(c);
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String1 s1 = new String1();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char c = sc.next().charAt(0);
+        System.out.println(s1.solution(str, c));
+    }
+}
